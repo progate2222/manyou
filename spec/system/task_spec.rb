00fb4f720df3092c46755c73137cefd9d07e9eb5
sayaka tamura <goooodnight@yahoo.co.jp>
2022-08-19 16:05:12 +0900

    タスクの並び順を降順にする

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe 'タスク管理機能', type: :system do
        end
      end
   end
+
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
@@ -19,7 +20,13 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task'
       end
     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        # ここに実装する
+      end
+    end
   end
+
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
@@ -30,4 +37,5 @@ RSpec.describe 'タスク管理機能', type: :system do
        end
      end
   end
+  
 end

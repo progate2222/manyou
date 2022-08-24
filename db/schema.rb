83273391847e21147cf78a1cf87da645bd4a2304
sayaka tamura <goooodnight@yahoo.co.jp>
2022-08-24 14:47:40 +0900

    nameに検索インデックスを貼る

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_090843) do
+ActiveRecord::Schema.define(version: 2022_08_24_054538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_08_22_090843) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "status", default: "未着手"
     t.integer "priority", default: 0
+    t.index ["name"], name: "index_tasks_on_name"
   end
 
 end

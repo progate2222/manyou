2bfa27421d26d6d465b6e817ed756571820364ad
sayaka tamura <goooodnight@yahoo.co.jp>
2022-08-24 18:21:42 +0900

    ユーザー登録画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'users#new'
+  resources :users, only: [:new, :create, :show]
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
